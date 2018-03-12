2d5f16693a74fb575ad84a09c91de0042ecec7d9
MaxSac <maximilian.sackel@gmx.de>
2018-03-12 13:26:18 +0100

    svm

--- 02-SupportVectorMachine/svm.py
+++ 02-SupportVectorMachine/svm.py
@@ -14,64 +14,74 @@ def make_data(n):
     return x_data, y_label
 
 
-def plot(data, label):
-    """Plots label-colored data (2D)"""
-    fig, ax = plt.subplots()
-    for lab, c in zip(np.unique(label), ['red', 'green']):
-        ax.scatter(
-            data[:, 0][(label == lab)],
-            data[:, 1][(label == lab)],
-            color=c,
-        )
-    print('Data plotted.')
-    return fig, ax
+class Plotter:
+    def __init__(self):
+        pass
 
+    def plot_label(self, data, label):
+        """Plots label-colored data (2D)"""
+        fig, ax = plt.subplots()
+        for lab, c in zip(np.unique(label), ['red', 'green']):
+            ax.scatter(
+                data[:, 0][(label == lab)],
+                data[:, 1][(label == lab)],
+                color=c,
+            )
+        print('Data plotted.')
+        return fig, ax
 
-def plot_hyperplane(w, b, data, label):
-    fig, ax = plot(data, label)
-    x = np.linspace(0, 3, len(label))
-    # ax.plot(x, (- w[0] * x + b) / w[1], label='"Normal"')
-    ax.plot(x, w[1] * x + b, label='"Vektor"')
-    fig.legend()
-    return fig, ax
+    def plot_hyperplane(self, w, b, data, label):
+        fig, ax = plot(data, label)
+        x = np.linspace(0, 3, len(label))
+        # ax.plot(x, (- w[0] * x + b) / w[1], label='"Normal"')
+        ax.plot(x, w[1] * x + b)
+        # fig.legend()
+        return fig, ax
 
 
 class SVM:
     """Support Vector Machine"""
-    def __init__(self):
+    def __init__(self, learning_rate=0.05, epochs=1):
         self.w = None  # Normalenvektor
         self.b = None  # y-Achsenabschnitt (Bias)
         self.alpha = None  # Lagrangemultiplikatoren
-
-    def gradient_normal(self, w, support, label, data):
-        return w - np.sum(np.dot(support*label,data), axis=0)
-
-    def gradient_bios(self, support, label):
-        return np.sum(support*label)
-
-    def gradient_support(self, label, data, w, bios):
-        return label*data*w 
+        self.learning_rate = learning_rate
+        self.epochs = epochs
     
+    def gradient_weight(self, data, label):
+        return self.w - np.dot((self.alpha * label), data)
+
+    def gradient_bias(self, data, label):
+        return -np.sum(self.alpha * label)
+
+    def gradient_alpha(self, data, label):
+        return label*(np.dot(data, self.w) + self.b) -1
+
+    def encode_labels(self, label):
+        u_label = np.unique(label)
+        label[u_label[0]] = -1
+        label[u_label[1]] = 1
+        return label
+
+    def estimate(self, train, label):
+        label = self.encode_labels(label)
+
+        self.w = np.ones(train.shape[1])
+        self.alpha = np.ones(train.shape[0])
+        self.alpha = self.alpha / np.linalg.norm(self.alpha)
+        self.b = np.ones(train.shape[1] - 1)
+
+        for i in range(self.epochs):
+            self.fit(train, label)
+
     def fit(self, train, label):
-        """Train SVM"""
-        learning_rate = 0.9
+        self.w -= self.learning_rate * self.gradient_weight(train, label)
+        self.w /= np.linalg.norm(self.w)
+        self.b -= self.learning_rate * self.gradient_bias(train, label)
+        self.alpha -= self.learning_rate * self.gradient_alpha(train, label)
+        print(self.gradient_alpha(train, label))
 
-        # Normalenvektor schätzen und normieren
-        new_w = self.w - learning_rate * (self.w - np.dot(label * self.alpha, train))
-        new_w = np.linalg.norm(new_w)
-
-        # Lagrangemultiplikatoren schätzen und normieren
-        new_alpha = self.alpha - learning_rate * label * (np.dot(self.w, np.transpose(train)) + self.b - 1)
-        new_alpha = new_alpha / np.sum(new_alpha)
-
-        # y-Achsenabschnitt schätzen und normieren
-        new_b = self.b - - learning_rate * np.dot(self.alpha, label)
-
-        self.w = new_w
-        self.alpha = new_alpha
-        self.b = new_b
-
-    def estimate(self, x):
+    def classify(self, data):
         """Classify data.
         Liegt der Punkt links oder rechts von der Trennebene?
         $sign(x \cdot w + b)$
@@ -80,21 +90,17 @@ class SVM:
         return classification
 
 
-def test_svm():
-    svm = SVM()
-    svm.w = np.array([1, 0])
-    svm.b = 0
-    svm.fit(x_data, y_label)
-    # print(svm.estimate(train) == label[:, 0])
+# def test_svm():
+#     svm = SVM()
+#     svm.estimate(x_data, y_label)
 
-def main():
+def main(number_samples):
     x_data, y_label = make_data(50)
+    svm = SVM()
+    svm.fit(x_data, y_label)
     fig, ax = plot(x_data, y_label)
-    # fig.savefig('train_svm.png')
+    fig.show()
 
-x_data, y_label = make_data()
-plot(x_data, y_label)
-test_svm()
 
 if __name__ == '__main__':
-    main()
+    main(number_samples)
--- 02-SupportVectorMachine/test_svm.py
+++ 02-SupportVectorMachine/test_svm.py
@@ -6,74 +6,69 @@ n = 50
 x_data, y_label = make_data(n)
 
 
-def environment():
+def environment(epochs=1):
     """
     Baue eine Umgebung aus Testzwecken.
     Diese Umgebung sollte immer gleich aussehen, aus Gründen der Einfachkeit.
     """
-    svm = SVM()
-
-    svm.w = np.array([1, 0])
-    svm.b = 0
-
+    svm = SVM(epochs=epochs)
     return svm
 
 
 def test_normalvector_shape():
     """Teste, ob Normalenvektor ein 2D-Vektor ist."""
     svm = environment()
-    assert svm.w.shape == (2,)
+    # Initialize Weights and Bias
+    svm.w = np.ones(x_data.shape[1])
+    svm.alpha = np.ones(x_data.shape[0])
+    gradient_weight = svm.gradient_weight(x_data, y_label)
+    assert gradient_weight.shape == (x_data.shape[1], )
 
 
-def test_bias_type():
-    """Teste, ob der Achsenabschnitt ein Skalar ist."""
-    svm = environment()
-    assert type(svm.b) == int
+# def test_bias_type():
+#     """Teste, ob der Achsenabschnitt ein Skalar ist."""
+#     svm = environment()
+#     assert type(svm.b) == int
 
 
-def test_estimation_dimension():
-    """Teste, ob die geschätzten Label die richtige Dimension haben."""
-    svm = environment()
-    assert svm.estimate(x_data).shape == y_label.shape
+# def test_estimation_dimension():
+#     """Teste, ob die geschätzten Label die richtige Dimension haben."""
+#     svm = environment()
+#     assert svm.estimate(x_data).shape == y_label.shape
 
 
-def test_prediction():
-    """Teste, ob die SVM eine Genauigkeit von `acc` übersteigt."""
-    acc = 0
-    svm = environment()
-    svm.fit(x_data, y_label)
-    assert np.mean(svm.estimate(x_data) == y_label) > acc
+# def test_prediction():
+#     """Teste, ob die SVM eine Genauigkeit von `acc` übersteigt."""
+#     acc = 0
+#     svm = environment()
+#     svm.estimate(x_data, y_label)
+#     assert np.mean(svm.estimate(x_data) == y_label) > acc
 
 
-def test(iterations):
+def func(iterations):
     """Diese Funktion wird bei `python test_svm.py` gerufen!"""
     print('Support Vector Machine (SVM):')
     print('=============================')
 
-    svm = environment()
-    svm.w = np.array([1, 1])
-    svm.b = 1
-    svm.alpha = np.ones(len(x_data))
+    svm = environment(iterations)
 
-    for i in range(iterations):
+    # for i in range(iterations):
         # print('Iteration {}'.format(i))
-        svm.fit(x_data, y_label)
+    svm.estimate(x_data, y_label)
 
-        # print('Normalenvektor', svm.w)
-        # print('Lagrangemultiplikatoren', svm.alpha)
-        # print('y-Achsenabschnitt', svm.b)
+    print('Normalenvektor', svm.w)
+    print('Lagrangemultiplikatoren', svm.alpha)
+    print('y-Achsenabschnitt', svm.b)
 
-    print(svm.b)
+    # fig, ax = plot_hyperplane(svm.w, svm.b, x_data, y_label)
+    # fig.savefig('hyperplane.png')
 
-    fig, ax = plot_hyperplane(svm.w, svm.b, x_data, y_label)
-    fig.savefig('hyperplane.png')
-
-    print('Prediction:')
-    print('-----------')
-    print(svm.estimate(x_data) == y_label)
-    print('Acc:', np.mean(svm.estimate(x_data) == y_label))
-    pass
+    # print('Prediction:')
+    # print('-----------')
+    # print(svm.estimate(x_data, y_label) == y_label)
+    # print('Acc:', np.mean(svm.estimate(x_data, y_label) == y_label))
+    # pass
 
 
 if __name__ == '__main__':
-    fire.Fire(test)
+    fire.Fire(func)
